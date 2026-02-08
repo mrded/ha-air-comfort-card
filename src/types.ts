2961eb608b7ea65dac43ac85f5d423531b59b183
Dmitry Demenchuk <dmitry@demench.uk>
2026-02-08 01:56:26 +0000

    add graphs

--- src/types.ts
+++ src/types.ts
@@ -8,14 +8,23 @@ export interface CardConfig {
   show_comfort_level?: boolean;
 }
 
+export interface EntityState {
+  state: string;
+  attributes: Record<string, any>;
+}
+
+export interface HistoryState {
+  state: string;
+  last_changed: string;
+  last_updated: string;
+}
+
 export interface HomeAssistant {
   states: {
-    [entity_id: string]: {
-      state: string;
-      attributes: Record<string, any>;
-    };
+    [entity_id: string]: EntityState;
   };
   callService: (domain: string, service: string, data?: any) => Promise<void>;
+  callApi: <T>(method: string, path: string) => Promise<T>;
 }
 
 export interface LovelaceCardConfig {
